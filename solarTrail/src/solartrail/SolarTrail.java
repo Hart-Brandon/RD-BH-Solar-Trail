1265b36c207b204af87e2ca30692cebd568d6ed9
BHart <BHart@192.168.1.10>
2015-05-11 21:51:18 -0600

    5/11/2015 - Added Ship.java

--- solarTrail/src/solartrail/SolarTrail.java
+++ solarTrail/src/solartrail/SolarTrail.java
@@ -6,6 +6,8 @@
 package solartrail;
 
 import byui.cit260.solarTrail.model.Player;
+import byui.cit260.solarTrail.model.Ship;
+import byui.cit260.solarTrail.model.VisitPlanetScene;
 
 /**
  *
@@ -17,6 +19,8 @@ public class SolarTrail {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+      //Start Test Player class
       Player playerOne = new Player();
       
       playerOne.setName("Steve Rogers");
@@ -24,7 +28,22 @@ public class SolarTrail {
       
       String playerInfo = playerOne.toString();
       System.out.println(playerInfo);
+      //End Test
       
+      //Start Test VisitPlanetScene class
+      VisitPlanetScene sceneOne = new VisitPlanetScene();
+      
+      sceneOne.setBlocked(true);
+      sceneOne.setDescription("Yay scene!");
+      sceneOne.setFoodAvailable(10);
+      sceneOne.setFuelAvailable(5);
+      sceneOne.setSymbol("None");
+      sceneOne.setTravelTime(21.5);
+      sceneOne.setWaterAvailable(15);
+      
+      String sceneInfo = sceneOne.toString();
+      System.out.println(sceneInfo);
+      //End Test
     }
     
 }
