ff02ba1d58bd0439964e84421adb380f4342e054
retro <retro@Rich>
2015-05-11 20:55:09 -0700

    Changing things up FOR SCIENCE - 5/11/15

--- solarTrail/src/solartrail/SolarTrail.java
+++ solarTrail/src/solartrail/SolarTrail.java
@@ -6,6 +6,7 @@
 package solartrail;
 
 import byui.cit260.solarTrail.model.Player;
+import byui.cit260.solarTrail.model.Ship;
 
 /**
  *
@@ -25,6 +26,20 @@ public class SolarTrail {
       String playerInfo = playerOne.toString();
       System.out.println(playerInfo);
       
+      Ship theKestrel = new Ship();
+              
+      theKestrel.setDescription("A Bird of Prey.");
+      theKestrel.setSize(30);
+      theKestrel.setStatus("Damaged plenty, but she'll fly true.");
+      theKestrel.setMinCrewCapacity(1);
+      theKestrel.setMaxCrewCapacity(10);
+      theKestrel.setActualCrewSize(5);
+      theKestrel.setCargoCapacity(500);
+      theKestrel.setSpeed(600);
+      
+      String shipInfo = theKestrel.toString();
+      System.out.println(shipInfo);
+                           
     }
     
 }
