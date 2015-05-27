20e682fac0792148f341f838f1b70e5083bf86fe
Hart-Brandon <brandonthart.gs@gmail.com>
2015-05-27 15:02:22 -0600

    huntForResources

--- solarTrail/src/buyi/cit260/solarTrail/control/HarvestControl.java
+++ solarTrail/src/buyi/cit260/solarTrail/control/HarvestControl.java
@@ -7,8 +7,32 @@ package buyi.cit260.solarTrail.control;
 
 /**
  *
- * @author BHart
+ * @author BHart & RDunn
  */
 public class HarvestControl {
     
+    public int huntForResources(int amountAvailable, int amountToHarvest, 
+            int numCrew, int actualCrew, int cargoSpaceLeft, int daysLeft) {
+        
+        if (numCrew > (actualCrew - 1)) {
+            return -1;
+        }
+        
+        if (amountAvailable < amountToHarvest) {
+            return -1;
+        }
+        
+        if (cargoSpaceLeft < amountToHarvest) {
+            return -1;
+        }
+        
+        int timeToHarvest = (int) ((amountToHarvest * 0.5) - 
+                ((amountToHarvest * 0.5) * (numCrew -1) * 0.1));
+        
+        if (timeToHarvest > daysLeft) {
+            return -1;
+        }
+        
+        return timeToHarvest;
+    }
 }
