94debf241f016b8353068f1297459be3006b982e
Hart-Brandon <brandonthart.gs@gmail.com>
2015-05-29 20:09:50 -0600

    GameControl

--- solarTrail/src/buyi/cit260/solarTrail/control/GameControl.java
+++ solarTrail/src/buyi/cit260/solarTrail/control/GameControl.java
@@ -10,5 +10,16 @@ package buyi.cit260.solarTrail.control;
  * @author BHart
  */
 public class GameControl {
-    
+    public int endGame(int difficultyLvl, int numDays, int totalResources, 
+            int daysLeft) {
+        if (numDays > daysLeft)
+            return -1;
+        
+        int resourcesLeft = (int) totalResources - (1 - (1 * difficultyLvl)) * numDays;
+        
+        if (resourcesLeft < 0) 
+            return -1;
+        
+        return resourcesLeft;
+    }
 }
