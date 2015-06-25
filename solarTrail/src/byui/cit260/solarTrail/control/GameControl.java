4d34c81f81ca734c450cae45552bd226fd33a58a
Hart-Brandon <brandonthart.gs@gmail.com>
2015-06-24 20:10:32 -0600

    Creating Map

--- solarTrail/src/byui/cit260/solarTrail/control/GameControl.java
+++ solarTrail/src/byui/cit260/solarTrail/control/GameControl.java
@@ -24,10 +24,35 @@ public class GameControl
 {
     private final String ERROR_MSG = "The value entered is invalid.";
 
+    public static InventoryItem[] getSortedInventoryList()
+    {
+        InventoryItem[] originalInventoryList = SolarTrail.getCurrentGame().getInventory();
+        
+        //make a copy
+        InventoryItem[] inventoryList = originalInventoryList.clone();
+        
+        //Using a bubblesort to sort inventoryList by name
+        InventoryItem tempInventoryItem;
+        for (int i = 0; i < inventoryList.length-1; i++)
+        {
+            for (int j = 0; j < inventoryList.length-1-i; j++)
+            {
+                if (inventoryList[j].getDescription().compareToIgnoreCase(inventoryList[j + 1].getDescription()) > 0)
+                {
+                    tempInventoryItem = inventoryList[j];
+                    inventoryList[j] = inventoryList[j + 1];
+                    inventoryList[j + 1] = tempInventoryItem;
+                }
+            }
+        }
+        
+        return inventoryList;
+    }
+
     public static void assignScenesToLocations(Map map, Scene[] scenes)
     {
         Location[][] locations = map.getLocations();
-        
+
         locations[0][0].setScene(scenes[SceneType.start.ordinal()]);
         locations[0][1].setScene(scenes[SceneType.space.ordinal()]);
         locations[0][2].setScene(scenes[SceneType.space.ordinal()]);
@@ -41,73 +66,73 @@ public class GameControl
         locations[0][10].setScene(scenes[SceneType.space.ordinal()]);
         locations[0][11].setScene(scenes[SceneType.space.ordinal()]);
         locations[0][12].setScene(scenes[SceneType.artifact.ordinal()]);
-        locations[1][0].setScene(scenes[SceneType.space.ordinal()]);
-        locations[1][1].setScene(scenes[SceneType.space.ordinal()]);
-        locations[1][2].setScene(scenes[SceneType.planet.ordinal()]);
-        locations[1][3].setScene(scenes[SceneType.space.ordinal()]);
-        locations[1][4].setScene(scenes[SceneType.space.ordinal()]);
-        locations[1][5].setScene(scenes[SceneType.planet.ordinal()]);
-        locations[1][6].setScene(scenes[SceneType.space.ordinal()]);
-        locations[1][7].setScene(scenes[SceneType.space.ordinal()]);
-        locations[1][8].setScene(scenes[SceneType.planet.ordinal()]);
-        locations[1][9].setScene(scenes[SceneType.space.ordinal()]);
-        locations[1][10].setScene(scenes[SceneType.space.ordinal()]);
+        locations[1][12].setScene(scenes[SceneType.space.ordinal()]);
         locations[1][11].setScene(scenes[SceneType.space.ordinal()]);
-        locations[1][12].setScene(scenes[SceneType.finish.ordinal()]);
+        locations[1][10].setScene(scenes[SceneType.planet.ordinal()]);
+        locations[1][9].setScene(scenes[SceneType.space.ordinal()]);
+        locations[1][8].setScene(scenes[SceneType.space.ordinal()]);
+        locations[1][7].setScene(scenes[SceneType.planet.ordinal()]);
+        locations[1][6].setScene(scenes[SceneType.space.ordinal()]);
+        locations[1][5].setScene(scenes[SceneType.space.ordinal()]);
+        locations[1][4].setScene(scenes[SceneType.planet.ordinal()]);
+        locations[1][3].setScene(scenes[SceneType.space.ordinal()]);
+        locations[1][2].setScene(scenes[SceneType.space.ordinal()]);
+        locations[1][1].setScene(scenes[SceneType.space.ordinal()]);
+        locations[1][0].setScene(scenes[SceneType.finish.ordinal()]);
     }
-    
+
     public enum Item
     {
+
         food,
         fuel,
         water;
     }
-    
+
     private static InventoryItem[] createInventoryList()
     {
         InventoryItem[] inventory = new InventoryItem[14];
-        
+
         InventoryItem food = new InventoryItem();
         food.setDescription("Food");
         food.setQuantityInStock(0);
         food.setRequiredAmount(0);
         inventory[Item.food.ordinal()] = food;
-        
+
         InventoryItem fuel = new InventoryItem();
         fuel.setDescription("Fuel");
         fuel.setQuantityInStock(0);
         fuel.setRequiredAmount(0);
         inventory[Item.fuel.ordinal()] = fuel;
-        
+
         InventoryItem water = new InventoryItem();
         water.setDescription("Water");
         water.setQuantityInStock(0);
         water.setRequiredAmount(0);
         inventory[Item.water.ordinal()] = water;
-        
+
         return inventory;
     }
-    
+
     public static void createNewGame(Player player)
     {
         Game game = new Game();
         SolarTrail.setCurrentGame(game);
-       /** 
-        game.setPlayer(player);
-        
-        InventoryItem[] inventoryList = GameControl.createInventoryList();
-        game.setInventory(inventoryList);
-        
-        Ship ship = new Ship();
-        game.setShip(ship);
-        
-        Map map = MapControl.createMap();
-        game.setMap(map);
-        
-        MapControl.moveActorsToStartingLocation(map); 
-        **/
+        /**
+         * game.setPlayer(player);
+         *
+         * InventoryItem[] inventoryList = GameControl.createInventoryList();
+         * game.setInventory(inventoryList);
+         *
+         * Ship ship = new Ship(); game.setShip(ship);
+         *
+         * Map map = MapControl.createMap(); game.setMap(map);
+         *
+         * MapControl.moveActorsToStartingLocation(map); 
+        *
+         */
     }
-    
+
     public static void saveGame()
     {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
@@ -175,24 +200,28 @@ public class GameControl
 
         return player;
     }
-    
+
     //Used when player is at the final stage of the outward journey
-    public int artifactDays() {
+    public int artifactDays()
+    {
         int artifactTime = 0;
         Scanner scanner = new Scanner(System.in);
         boolean valid = false;
 
         //Loops until user inputs valid int value
-        while (valid == false) {
+        while (valid == false)
+        {
             System.out.println("The radiation from the artifact will overwhelm your"
                     + "\n  shields in 60 days.  You cannot stay longer than that."
                     + "\n\n How many days would you like to stay?");
 
             //Checks whether the input is a valid int between 0 and 30
-            if (artifactTime < 0 || artifactTime > 60) {
+            if (artifactTime < 0 || artifactTime > 60)
+            {
                 System.out.println(ERROR_MSG);
                 break;
-            } else {
+            } else
+            {
                 artifactTime = scanner.nextInt();
             }
         }
--- solarTrail/src/byui/cit260/solarTrail/view/InGameView.java
+++ solarTrail/src/byui/cit260/solarTrail/view/InGameView.java
@@ -7,6 +7,9 @@ package byui.cit260.solarTrail.view;
 
 import java.util.Scanner;
 import byui.cit260.solarTrail.control.GameControl;
+import byui.cit260.solarTrail.model.InventoryItem;
+import byui.cit260.solarTrail.model.Location;
+import byui.cit260.solarTrail.model.Map;
 import solartrail.SolarTrail;
 
 /**
@@ -15,6 +18,8 @@ import solartrail.SolarTrail;
  */
 public class InGameView extends View{
 
+    private static Map map = new Map();
+    
     public InGameView()
     {
         super("\n"
@@ -31,9 +36,26 @@ public class InGameView extends View{
 
         value = value.toUpperCase(); //convert to all upper case
         char choice = value.charAt(0); //get first character entered
+        
+        Location[][] locations = map.getLocations();
 
         switch (choice)
         {
+            case 'V':
+                this.displayMap(locations);
+                break;
+            case 'I': //View Inventory
+                this.viewInventory();
+                break;
+            case 'C': //View Crew
+                this.viewCrew();
+                break;
+            case 'T': //Travel
+                this.travel();
+                break;
+            case 'R': //Repare Ship
+                this.repairShip();
+                break;
             case 'S': //New Game
                 this.saveGame();
                 break;
@@ -64,6 +86,58 @@ public class InGameView extends View{
     }
 
     private void helpMenu()
+    {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void displayMap(Location[][] locations)
+    {
+        System.out.println("\t_____ _               _____                      "
+                         + "\t/  ___| |             |  _  |                     "
+                         + "\t\\ `--.| |_ __ _ _ __  | | | | ___ ___  __ _ _ __  "
+                         + "\t `--. \\ __/ _` | '__| | | | |/ __/ _ \\/ _` | '_ \\ "
+                         + "\t/\\__/ / || (_| | |    \\ \\_/ / (_|  __/ (_| | | | |"
+                         + "\t\\____/ \\__\\__,_|_|     \\___/ \\___\\___|\\__,_|_| |_|"
+                         + "\t                                                  ");
+        
+        for (int i = 0; i < locations.length; i++)
+        {
+            for (int j = 0; j < locations.length; j++)
+            {
+                
+            }
+        }
+    }
+
+    private void viewInventory()
+    {
+        //get the sorted list of inventory items for the current game
+        InventoryItem[] inventory = GameControl.getSortedInventoryList();
+        
+        System.out.println("\nList of Inventory Items");
+        System.out.println("Description\tRequired\tIn Stock");
+        
+        //for each inventory item
+        for (InventoryItem inventoryItem : inventory)
+        {
+            //DISPLAY the desc, required amount, amount in stock
+            System.out.println(inventoryItem.getDescription() + "\t"
+            + inventoryItem.getRequiredAmount() + "\t"
+            + inventoryItem.getQuantityInStock());
+        }
+    }
+
+    private void viewCrew()
+    {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void travel()
+    {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void repairShip()
     {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
